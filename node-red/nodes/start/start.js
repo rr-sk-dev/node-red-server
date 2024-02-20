2f0a0c854d6d537c56b183d6e6839a868a9765f2
rtsrodrigues <ruben.rodrigues@productdock.com>
2024-02-20 11:39:06 +0000

    chore: restore start node

--- node-red/nodes/start/start.js
+++ node-red/nodes/start/start.js
@@ -1,18 +1,10 @@
+const { buildMessageObject } = require('../utils/msg.utils');
+
 module.exports = function (RED) {
   function StartNode(config) {
     RED.nodes.createNode(this, config);
 
     this.theme = RED.nodes.getNode(config.theme);
-
-    const interval = setInterval(() => {
-      console.log(`\n[${config.type}] msg`, { store: { ready: true } }, '\n');
-      this.send({ store: { ready: true } });
-
-      const timeout = setTimeout(() => {
-        clearInterval(interval);
-        clearTimeout(timeout);
-      }, 1000 * 60);
-    }, 1000 * 5);
   }
 
   RED.nodes.registerType('start', StartNode);
